c0c4802500214fb7a70a9617e4503db4a9ed858a
Mykola Mysiv <mukolamusiv@gmail.com>
2025-04-28 22:24:05 +0300

    Create WarehousesRelationManager.php

---
+++ app/Filament/Resources/MaterialResource/RelationManagers/WarehousesRelationManager.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources\MaterialResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class WarehousesRelationManager extends RelationManager
+{
+    protected static string $relationship = 'warehouses';
+
+    protected static ?string $recordTitleAttribute = 'quantity';
+    protected static ?string $title = 'Залишки на складі';
+    protected static ?string $pluralLabel = 'Склади';
+    protected static ?string $navigationLabel = 'Склади';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('quantity')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('quantity')
+            ->columns([
+                Tables\Columns\TextColumn::make('material.name')
+                    ->label('Назва матеріалу')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('warehouse.name')
+                    ->label('Назва складу')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('quantity')
+                    ->label('Кількість')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('price')
+                    ->label('Ціна')
+                    ->money('UAH', true)
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('total_price')
+                    ->label('Загальна вартість')
+                    ->getStateUsing(fn ($record) => $record->quantity * $record->price.'.00')
+                    ->money('UAH', true)
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
