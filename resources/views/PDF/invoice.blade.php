e1469412ed3dbf74974572f066e09f481ad0f25b
Mykola Mysiv <mukolamusiv@gmail.com>
2025-05-06 00:48:48 +0300

    Update invoice.blade.php

--- resources/views/PDF/invoice.blade.php
+++ resources/views/PDF/invoice.blade.php
@@ -81,7 +81,7 @@
             @foreach ($invoice->invoiceItems as $index => $item)
                 <tr>
                 <td>{{ $index + 1 }}</td>
-                <td>{{ $item->description }}</td>
+                <td>{{ $item->material->name }}</td>
                 <td>{{ $item->quantity }}</td>
                 <td>{{ number_format($item->price, 2) }}</td>
                 <td>{{ number_format($item->total, 2) }}</td>
