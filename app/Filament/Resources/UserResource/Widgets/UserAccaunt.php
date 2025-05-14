64b5a70a911e9ccc5e7b2cdda7773f641e6dc250
Mykola Mysiv <mukolamusiv@gmail.com>
2025-05-14 13:23:56 +0300

    Update UserAccaunt.php

--- app/Filament/Resources/UserResource/Widgets/UserAccaunt.php
+++ app/Filament/Resources/UserResource/Widgets/UserAccaunt.php
@@ -55,7 +55,13 @@ class UserAccaunt extends BaseWidget
                             ->required()
                             ->default($this->account->id),
                     ])
-                    ->url('/transactions/create')
+                    ->action(function (array $data) {
+                        dd($data);
+                        // $this->record->account->balance = $this->record->account->balance - 100;
+                        // $this->record->account->save();
+                        // $this->record->account->refresh();
+                        // dd($this->record);
+                    })
                     ->icon('heroicon-o-currency-dollar')
                     ->color('success')
                 ),
