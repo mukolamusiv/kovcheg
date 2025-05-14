0c9994e8ee5c309f037e52ce4241602fbc0c887b
Mykola Mysiv <mukolamusiv@gmail.com>
2025-05-14 13:09:34 +0300

    Update UserAccaunt.php

--- app/Filament/Resources/UserResource/Widgets/UserAccaunt.php
+++ app/Filament/Resources/UserResource/Widgets/UserAccaunt.php
@@ -30,13 +30,12 @@ class UserAccaunt extends BaseWidget
 
         //dd($balance);
 
-        dd($this->account->balance);
+      //  dd($this->account->balance);
         return [
            // Stat::make('Total Products', $this->record->production->count()),
                 //->icon('heroicon-o-archive'),
             Stat::make('На рахунку', $this->account->balance)
-                ->description('32k increase')
-                ->descriptionIcon('heroicon-m-arrow-trending-up'),
+                ->description('32k increase'),
                 //->icon('heroicon-o-shopping-cart'),
             //Stat::make('Total Transactions', $this->record->transactions->count()),
                 //->icon('heroicon-o-currency-dollar'),
