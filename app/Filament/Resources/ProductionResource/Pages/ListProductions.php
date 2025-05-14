585413925651f3b75cf8236d800bc36a22d035f1
Mykola Mysiv <mukolamusiv@gmail.com>
2025-05-14 12:59:05 +0300

    ів

--- app/Filament/Resources/ProductionResource/Pages/ListProductions.php
+++ app/Filament/Resources/ProductionResource/Pages/ListProductions.php
@@ -5,7 +5,8 @@ namespace App\Filament\Resources\ProductionResource\Pages;
 use App\Filament\Resources\ProductionResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
-
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
 class ListProductions extends ListRecords
 {
     protected static string $resource = ProductionResource::class;
@@ -16,4 +17,21 @@ class ListProductions extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
+
+    public function getTabs(): array
+    {
+        $array = [
+            'Створені' => Tab::make()
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('status', '=', 'створені')),
+            'В роботі' => Tab::make()
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('status', '=', 'в роботі')),
+            'Виготовлені' => Tab::make()
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('status', '=', 'виготовлено')),
+            'Скасовані' => Tab::make()
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('status', '=', 'скасовано')),
+            'Вся продукція' => Tab::make(),
+        ];
+        return $array;
+    }
+
 }
