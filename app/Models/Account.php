7e351047613fcacd93cfca290595091cedcbad53
Mykola Mysiv <mukolamusiv@gmail.com>
2025-05-30 20:48:56 +0300

    Update Account.php

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -66,7 +66,7 @@ class Account extends Model
             $this->balance = $pay - $balans;// + $paidUser;
             $this->save();
             if($customer->id == 4 ){
-                dd($pay - $balans + $paidUser,$customer->id, $customer->calculateOutstandingInvoices(), $pay, $paidUser, $balans, $this->balance, $debitSum, $creditSum);
+                dd($balans - $pay,$customer->id, $customer->calculateOutstandingInvoices(), $pay, $paidUser, $balans, $this->balance, $debitSum, $creditSum);
             }
         }
 
