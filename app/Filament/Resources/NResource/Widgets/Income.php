73ee5588990502defdca2994bf17ca4dbb6f3b74
Mykola Mysiv <mukolamusiv@gmail.com>
2025-06-23 15:27:53 +0300

    Update Income.php

--- app/Filament/Resources/NResource/Widgets/Income.php
+++ app/Filament/Resources/NResource/Widgets/Income.php
@@ -2,25 +2,54 @@
 
 namespace App\Filament\Resources\NResource\Widgets;
 
+use App\Models\Invoice;
+use App\Models\Production;
 use Filament\Widgets\ChartWidget;
 
 class Income extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+    protected static ?string $heading = 'Чистий дохід';
+
+
+    public function calculateTotalIncome()
+    {
+        $salesInvoices = Invoice::selectRaw('SUM(total) as total, MONTH(invoice_date) as month')
+            ->groupBy('month')
+            ->where('type', 'продаж')
+            ->pluck('total', 'month');
+
+        $purchaseInvoices = Production::selectRaw('SUM(price * quantity) as total, MONTH(production_date) as month')
+            ->groupBy('month')
+            ->pluck('total', 'month');
+
+
+        $totalIncome = [];
+
+        foreach (range(1, 12) as $month) {
+            $sales = $salesInvoices[$month] ?? 0;
+            $purchases = $purchaseInvoices[$month] ?? 0;
+            $totalIncome[$month] = $sales - $purchases;
+        }
+
+        return $totalIncome;
+    }
 
     protected function getData(): array
     {
-        return [
-            'datasets' => [
-                    [
-                        'label' => 'Blog posts created',
-                        'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
-                        'backgroundColor' => '#36A2EB',
-                        'borderColor' => '#9BD0F5',
-                    ],
-                ],
-                'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        ];
+
+        return $this->calculateTotalIncome();
+
+        // return [
+        //     'datasets' => [
+        //         [
+        //             'label' => 'Blog posts created',
+        //             'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+        //             'backgroundColor' => '#36A2EB',
+        //             'borderColor' => '#9BD0F5',
+        //         ],
+        //     ],
+        //             'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        // ];
     }
 
     protected function getType(): string
