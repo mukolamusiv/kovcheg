953433e7bf8979736876bb2121cf7277dd6954b5
Mykola Mysiv <mukolamusiv@gmail.com>
2025-07-02 01:04:18 +0300

    Update ProductionStagesRelationManager.php

--- app/Filament/Resources/UserResource/RelationManagers/ProductionStagesRelationManager.php
+++ app/Filament/Resources/UserResource/RelationManagers/ProductionStagesRelationManager.php
@@ -36,9 +36,8 @@ class ProductionStagesRelationManager extends RelationManager
                 Tables\Columns\TextColumn::make('name')->label('Назва'),
                 Tables\Columns\TextColumn::make('description')->label('Опис'),
                 Tables\Columns\TextColumn::make('status')->label('Статус'),
-                Tables\Columns\TextColumn::make('production_id')->label('ID виробництва'),
-                Tables\Columns\TextColumn::make('user_id')->label('ID користувача'),
-                Tables\Columns\TextColumn::make('date')->label('Дата')->date(),
+                Tables\Columns\TextColumn::make('production.name')->label('Виробництво'),
+                Tables\Columns\TextColumn::make('updated_at')->label('Дата')->date(),
                 Tables\Columns\BooleanColumn::make('paid_worker')->label('Оплачений працівник'),
             ])
             ->filters([
