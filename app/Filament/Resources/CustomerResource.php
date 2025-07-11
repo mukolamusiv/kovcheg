42359078847dbcf66cbb6fcd7dac8cfe0587b398
Mykola Mysiv <mukolamusiv@gmail.com>
2025-07-11 16:05:42 +0300

    Update CustomerResource.php

--- app/Filament/Resources/CustomerResource.php
+++ app/Filament/Resources/CustomerResource.php
@@ -182,22 +182,23 @@ class CustomerResource extends Resource
 
 
 
-                        Forms\Components\Repeater::make('size')
-                        ->relationship('size')
-                        ->schema([
-                            Forms\Components\TextInput::make('length')
-                                ->label('Ріст людини')
-                                ->nullable(),
-                            Forms\Components\TextInput::make('throat')
-                                ->label('Шия ')
-                                ->nullable(),
-                            Forms\Components\TextInput::make('comment')
-                                ->label('Примітка')
-                                ->nullable(),
-                        ])
-                        ->label('Розміри клієнта')
-                        ->maxItems(1)
-                        ->columns(2),
+                        Forms\Components\Repeater::make('size_customer_sizes')
+                            ->relationship('size')
+                            ->schema([
+                                Forms\Components\TextInput::make('length')
+                                    ->label('Ріст людини')
+                                    ->nullable(),
+                                Forms\Components\TextInput::make('throat')
+                                    ->label('Шия ')
+                                    ->nullable(),
+                                Forms\Components\TextInput::make('comment')
+                                    ->label('Примітка')
+                                    ->nullable(),
+                            ])
+                            ->label('Розміри клієнта')
+                            ->maxItems(1)
+                            ->minItems(1)
+                            ->columns(2),
 
             ]);
     }
